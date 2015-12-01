b7229c8b53dd09a18d4a99583fac71ab4acc02dc
lyricalpanda <lemmings@gmail.com>
2015-12-01 14:44:01 -0800

    Initial Commit

---
+++ RefreshControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RefreshControl
+//
+//  Created by Eric Harmon on 12/1/15.
+//  Copyright © 2015 Eric Harmon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RefreshControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RefreshControl
+//
+//  Created by Eric Harmon on 12/1/15.
+//  Copyright © 2015 Eric Harmon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
